eb392ea37d4d451cbc5e0f13b2ce11bba3082f4d
SolidGoose <154474121+SolidGoose@users.noreply.github.com>
2025-12-01 17:48:34 +0200

    Do some formatting

--- Scripts/mushroom.gd
+++ Scripts/mushroom.gd
@@ -4,18 +4,20 @@ const tile_size: Vector2 = Vector2(16, 16)
 var sprite_node_pos_tween: Tween
 var tml: TileMapLayer
 
+@export var lifes: int
+
 # ice = (8, 1), grey/floor = (1,7), wall = (1,1)
-const ICE_CELL_TYPE = Vector2i(8,1)
-const FLOOR_CELL_TYPE = Vector2i(1,7)
-const WALL_CELL_TYPE = Vector2i(1,1)
+const ICE_CELL_TYPE = Vector2i(8, 1)
+const FLOOR_CELL_TYPE = Vector2i(1, 7)
+const WALL_CELL_TYPE = Vector2i(1, 1)
 
 func _ready() -> void:
 	tml = get_parent().get_node("TileMapLayer")
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if !sprite_node_pos_tween or not sprite_node_pos_tween.is_running():
-		var dir = Vector2(0,0)
-		var pos = Vector2(0,0)
+		var dir = Vector2(0, 0)
+		var pos = Vector2(0, 0)
 		if Input.is_action_just_pressed("Up") and !$Up.is_colliding():
 			dir = Vector2(0, -1)
 			pos = tml.local_to_map(global_position)
@@ -28,17 +30,17 @@ func _physics_process(delta: float) -> void:
 		elif Input.is_action_just_pressed("Right") and !$Right.is_colliding():
 			dir = Vector2(1, 0)
 			pos = tml.local_to_map(global_position)
-		
+
 		if is_ice_next(dir, pos):
 			print("is ice next")
-			var len = get_ice_length(dir, pos)
-			var speed = 0.2*(len+1) # change 0.2 to what speed you want
-			dir = dir*(len+1) # this is hacky i know
+			var ice_length = get_ice_length(dir, pos)
+			var speed = 0.2 * (ice_length + 1) # change 0.2 to what speed you want
+			dir = dir * (ice_length + 1) # this is hacky i know
 			_move(dir, speed)
 		else:
-			if dir != Vector2(0,0):
+			if dir != Vector2(0, 0):
 				_move(dir)
-		
+
 func is_ice(pos: Vector2):
 	var cell_type = tml.get_cell_atlas_coords(pos)
 	if cell_type != Vector2i(-1, -1):
@@ -46,35 +48,37 @@ func is_ice(pos: Vector2):
 	return false
 	
 func is_ice_next(dir: Vector2, currpos: Vector2):
-	var pos = Vector2(dir.x+currpos.x, dir.y+currpos.y)
+	var pos = Vector2(dir.x + currpos.x, dir.y + currpos.y)
 	return is_ice(pos)
 
 func get_ice_length(dir: Vector2, currpos: Vector2):
-	var i = currpos.x
-	var j = currpos.y
-	var len = 0
-	print(i,j)
+	var i: float = currpos.x
+	var j: float = currpos.y
+	var ice_length: int = 0
+	print(i, j)
 	while i >= 0 and j >= 0:
 		i = i + dir.x
 		j = j + dir.y
-		var cell_type = tml.get_cell_atlas_coords(Vector2i(i, j))
+		var cell_type = tml.get_cell_atlas_coords(Vector2(i, j))
 		if cell_type == Vector2i(-1, -1) or cell_type != ICE_CELL_TYPE:
-			return len
-			#print('0')
-		else: 
+			return ice_length
+		else:
 			if cell_type == ICE_CELL_TYPE:
-				len += 1
-			#print('1')
-	return len
+				ice_length += 1
+	return ice_length
 
-func _move(dir: Vector2, speed: float=0.2):
+func _move(dir: Vector2, speed: float = 0.2):
+	# Define walk direction and distance
 	var walk_vector: Vector2 = dir * tile_size
+	# Istantly move the whole body to the new position
 	global_position += walk_vector
+	# Move the sprite back to where it was 
 	$AnimatedSprite2D.global_position -= walk_vector
 
 	if sprite_node_pos_tween:
 		sprite_node_pos_tween.kill()
-	
+
+	# Create a new tween to move the sprite to the new position 
 	sprite_node_pos_tween = create_tween()
 	sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	sprite_node_pos_tween.tween_property($AnimatedSprite2D, "global_position", global_position, speed).set_trans(Tween.TRANS_SINE)
